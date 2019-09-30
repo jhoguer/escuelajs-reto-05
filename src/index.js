46b04ce02e7744716257d0cd723b485a276c765d
Jhon Guerrero <jhoguer@gmail.com>
2019-09-30 16:24:29 -0500

    Solucion del primer problema

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+//const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters'
+
+if(localStorage.getItem('next_fetch')) {
+  //localStorage.removeItem('next_fetch');
+  localStorage.clear();
+}
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      localStorage.setItem('next_fetch', response.info.next);
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -23,13 +30,25 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = (nextPage = API) => {
+  if(!nextPage) {
+    alert('Primer llamdo API')
+    getData(API);
+  } else {
+    alert(nextPage)
+    getData(nextPage)
+  }
+  //localStorage.removeItem('next_fetch');
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
+const intersectionObserver = new IntersectionObserver(entries => {  
+  const nextPage = localStorage.getItem('next_fetch');
+
   if (entries[0].isIntersecting) {
-    loadData();
+    loadData(nextPage);    
+  }
+  if(nextPage === "") {
+    return
   }
 }, {
   rootMargin: '0px 0px 100% 0px',
